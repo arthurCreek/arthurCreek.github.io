6db254dc175695c4e49d5cc22ff4ff621f4e0109
Artie <turo310@gmail.com>
2019-10-22 20:20:37 -0700

    Test index.js mvc structure with Search.js model and searchView.js View

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 // Global app controller
-import num from './test';
-const x = 23;
 
-console.log('Testing testing again!');
-console.log(`We imported ${num} and variable x is ${x}`);
+import string from './models/Search';
+
+// import { add, multiply, ID } from './views/searchView';
+// console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3, 5)} and ${string}`);
+
+import * as searchView from './views/searchView';
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)} and ${string}`);
