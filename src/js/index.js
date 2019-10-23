11f730975223754ef421162432bb52efba0ff481
Artie <turo310@gmail.com>
2019-10-22 21:56:28 -0700

    Start main controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,17 @@
 // Global app controller
+import Search from './models/Search';
 
-import string from './models/Search';
 
-// import { add, multiply, ID } from './views/searchView';
-// console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3, 5)} and ${string}`);
+/**
+ * Global state of the app
+ *  - Search object
+ *  - Current recipe object
+ *  - Shopping list object
+ *  - Liked recipes
+ */
+const state = {};
 
-import * as searchView from './views/searchView';
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)} and ${string}`);
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'I am an exported string.';
+// Use axios to do AJAX call instead of fetch to be compatible with all browsers
+// Unlike fetch, axios return the json immedietally
+import axios from 'axios';
+import { appKey, appID} from '../secret';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        const baseURL = 'https://api.edamam.com'
+        try {
+            const res = await axios(`${baseURL}/search?q=${this.query}&app_id=${appID}&app_key=${appKey}`);
+            this.result = res.data.hits[0].recipe;
+            // console.log(this.result);
+        } catch(error) {
+            alert(error);
+        }
+    }
+}
+
