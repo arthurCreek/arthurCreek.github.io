3e0c976cb8c97db7c7d80cebebba0732e3c24c6a
Artie <turo310@gmail.com>
2019-10-24 00:05:59 -0700

    Recipe in side bar is highlighted when chosen

--- src/js/index.js
+++ src/js/index.js
@@ -75,6 +75,9 @@ const controlRecipe = async () => {
         recipeView.clearRecipe();
         renderLoader(elements.recipe);
 
+        // Highlight selected recipe
+        if(state.search) searchView.highlightSelected(id);
+
         // Create new recipe object
         state.recipe = new Recipe(id.replace('#', ""));
         console.log(state.recipe);
